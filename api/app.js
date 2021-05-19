5aa6874965af81a2aa9cfea9757392a10ddfef5e
zeddoth <oblidark@gmail.com>
2021-05-19 13:58:09 +0200

    derniere version stable et sécu

--- api/app.js
+++ api/app.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser"); Inutile avec la dernière version d'express
 const mongoose = require("mongoose");
 const mongoSanitize = require("express-mongo-sanitize");
-//RATE LIMIT
+//Rate Limit
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limité chaque IP à 100 requêtes par windowMs
+  max: 100, // limité chaque IP à 100 requêtes / windowMs (Cette sécurité est bien souvent mis en place par l'hébergeur)
 });
 //
 const helmet = require("helmet");
@@ -19,7 +19,7 @@ const host = process.env.DB_HOST;
 const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
 
-//CONNEXION MONGODB
+//Connexion MongoDB
 mongoose
   .connect(`mongodb+srv://${username}:${password}@${host}`, {
     useNewUrlParser: true,
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 app.use(limiter);
-app.use(bodyParser.json());
+// app.use(bodyParser.json()); Inutile avec la dernière version d'express
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use("/api/sauces", stuffRoutes);
 app.use("/api/auth", userRoutes);
--- api/routes/stuff.js
+++ api/routes/stuff.js
@@ -7,8 +7,8 @@ const multer = require("../middleware/multer-config");
 router.post("/", auth, multer, stuffController.createSauce);
 router.put("/:id", auth, multer, stuffController.modifySauce);
 router.delete("/:id", auth, stuffController.deleteSauce);
-router.get("/:id", stuffController.getOneSauce);
-router.get("/", stuffController.getAllSauces);
+router.get("/:id", auth, stuffController.getOneSauce);
+router.get("/", auth, stuffController.getAllSauces);
 router.post("/:id/like", auth, stuffController.likeSauce);
 
 module.exports = router;
